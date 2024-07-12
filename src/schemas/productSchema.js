744530307191072857d7d47f560ed4d733ddcc1d
BayajidAlam <bayajidalam2001@gmail.com>
2024-07-13 02:41:43 +0600

    chore: backend task server and description done

--- src/schemas/productSchema.js
+++
@@ -1,9 +0,0 @@
-const yup = require('yup');
-
-const productSchema = yup.object().shape({
-  productName: yup.string().required("Product Name is required"),
-  price: yup.number().positive().required(),
-  description: yup.string(),
-});
-
-module.exports = { productSchema };
