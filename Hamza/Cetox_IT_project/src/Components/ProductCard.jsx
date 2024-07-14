b92327d993771bf72e9e8ecdb1da7e78dd3143fb
smile995 <ah.smile995@gmai.com>
2024-07-14 10:58:45 +0600

    changes some css property

--- Hamza/Cetox_IT_project/src/Components/ProductCard.jsx
+++ Hamza/Cetox_IT_project/src/Components/ProductCard.jsx
@@ -4,19 +4,14 @@ import "../../src/index.css";
 import { Link } from 'react-router-dom';
 // eslint-disable-next-line react/prop-types
 const ProductCard = ({ product }) => {
-    const { _id, productName, price, sku, shopOwner } = product
+    const { _id, productName, price } = product
     return (
-        <div className='productCardDesign bg-slate-200 space-y-2 '>
+        <div className=' p-5 rounded-lg bg-slate-100 shadow-md space-y-2 '>
             <h2 className='text-xl'>Product Name: {productName}</h2>
             <h2 className='text-xl'>Price: {price}</h2>
-            <h2 className='text-xl'>Shop Owner: {shopOwner}</h2>
-            <div className='flex justify-between'>
-                <p className='text-lg font-semibold '>Product Size: {sku.size}</p>
-                <p className='text-lg font-semibold '>Product Color: {sku.color}</p>
-            </div>
             <div className='flex justify-center w-full'>
                 <Link to={_id}>
-                    <button className='px-3 py-1 bg-green-700 text-white w-full ' >Details</button>
+                    <button className='px-3 rounded py-1 bg-green-700 text-white w-full ' >Details</button>
                 </Link>
             </div>
         </div>
--- Hamza/Cetox_IT_project/src/Pages/Details.jsx
+++ Hamza/Cetox_IT_project/src/Pages/Details.jsx
@@ -3,19 +3,34 @@ import { useParams } from "react-router-dom";
 
 
 const Details = () => {
-    const [product,setProduct]=useState(null)
-    const {id}= useParams()
-    useEffect(()=>{
+    const [product, setProduct] = useState(null)
+    const { id } = useParams()
+    useEffect(() => {
         fetch(`https://github-react-task.vercel.app/products/${id}`)
-        .then(res=>res.json())
-        .then(data=>setProduct(data?.data))
-    },[id])
+            .then(res => res.json())
+            .then(data => setProduct(data?.data))
+    }, [id])
     console.log(product);
- 
+
     return (
-        <div>
-            product details
+    <>
+        <h1 className="text-5xl font-semibold text-center text-gray-800 mt-9">Product Details Page</h1>
+        <div className='  flex justify-center items-center  my-9'>
+            <div className="lg:w-1/2 bg-gray-100  p-10 rounded-lg shadow-lg ">
+
+                <h2 className='text-xl'>Product Name: {product?.productName}</h2>
+                <h2 className='text-xl'>Price: {product?.price} </h2>
+                <h2 className='text-xl'>Shop Owner: {product?.shopOwner} </h2>
+                <div className='flex justify-between'>
+                    <p className='text-lg font-semibold '>Product Size: {product?.sku?.size}</p>
+                    <p className='text-lg font-semibold '>Product Color: {product?.sku?.color} </p>
+                </div>
+                <div className='flex justify-center w-full'>
+
+                </div>
+            </div>
         </div>
+    </>
     );
 };
 
